92a1a06f790bef3dfe7047c71c65a6a174fbe25a
narendrareddy8074 <mnreddy659@gmail.com>
2025-02-06 10:14:35 +0530

    Print array elements

---
+++ Demo065.java
@@ -0,0 +1,11 @@
+//PRINT ARRAY ELEMENTS USING FOR EACH LOOP
+
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        for (int i : arr) {
+            System.out.println(i + " ");
+        }
+    }
+    
+}
